7e1916175fb75f9c742133a6f322120a573e1fa4
justinrothaug <94494722+justinrothaug@users.noreply.github.com>
2024-04-17 11:52:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,7 +79,7 @@ def get_chatassistant_chain():
     #text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100, separator = "\n",length_function=len)
     #texts = text_splitter.split_documents(documents)
     
-    embeddings_model = OpenAIEmbeddings(openai_api_key=client)
+    embeddings_model = OpenAIEmbeddings()
     #vectorstore = FAISS.from_documents(texts, embeddings_model)
     vectorstore = PineconeVectorStore(index_name="justinai", embedding=embeddings_model)
     #llm = ChatOpenAI(model="ft:gpt-3.5-turbo-0125:personal::92WRQSTH", temperature=1)
