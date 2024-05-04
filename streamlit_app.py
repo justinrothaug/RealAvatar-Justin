7da6b944e0e4db6cbc47829b7416875944533c15
justinrothaug <94494722+justinrothaug@users.noreply.github.com>
2024-05-04 09:41:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,7 +114,7 @@ Question: {question}
 
 # Define the columns we want to embed vs which ones we want in metadata
 # In case we want different Prompts for GPT and Llama
-Prompt_GPT = PromptTemplate(template=GPT_prompt_template, input_variables=["question"])
+Prompt_GPT = PromptTemplate(template=GPT_prompt_template, input_variables=["question", "context", "system", "chat_history"])
 Prompt_Llama = PromptTemplate(template=Llama_prompt_template, input_variables=["question", "context", "system", "chat_history"])
 
 
